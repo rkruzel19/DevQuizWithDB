862ee4322c49917802d397ede72ba9f25ff5b313
Rob Kruzel <rkruzel576@gmail.com>
2022-07-15 13:40:46 -0400

    Got the project downloaded on local computer. Installed required tools to make everything run (jdk, javafx, sql). Created a new SQL DB and connected it. Everything is running properly and ready to continue building.

--- src/main/java/app/services/ConnectionUtil.java
+++ src/main/java/app/services/ConnectionUtil.java
@@ -8,7 +8,7 @@ public class ConnectionUtil {
     public static Connection conDB(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizdb?useSSL=false", "root", "hockey");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizdb?useSSL=false", "root", "password");
             System.out.println("Connected");
             return con;
         } catch (Exception ex) {
